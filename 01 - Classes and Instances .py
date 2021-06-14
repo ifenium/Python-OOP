eadd385fa606fc1e204559b6d07fcb0586ea7a0d
Ifeoluwa Oyelakin <oyeife@gmail.com>
2021-06-14 23:41:02 +0100

    Update 01 - Classes and Instances .py

--- 01 - Classes and Instances .py
+++ 01 - Classes and Instances .py
@@ -3,25 +3,19 @@
 # the employee class would be an instance of that class.  
 
 class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
     pass
 
 # Each of these will be their own unique instances of the employee class
-emp1 = Employee()
-emp2 = Employee() 
+emp1 = Employee('ife', 'oye', '$50,000')
+emp2 = Employee('efi', 'eyo', '$80,000') 
 
-print(emp1)
-print(emp2)
-
-# Instance variables contains data that is unique to each instance 
-emp1.first = 'ife'
-emp1.last = 'oye'
-emp1.email = 'oyeife@gmail.com'
-emp1.pay = '$50,000'
-
-emp2.first = 'efi'
-emp2.last = 'eyo'
-emp2.email = 'efieyo@gmail.com'
-emp2.pay = '$80,000'
+# print(emp1)
+# print(emp2)
 
 print(emp1.email)
 print(emp2.email)
