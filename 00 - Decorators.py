13f4fc3e3285a6b542258b79707b8ffbc3878501
Ifeoluwa Oyelakin <oyeife@gmail.com>
2021-09-13 12:50:57 +0100

    Fixed my_timer function

--- 00 - Decorators.py
+++ 00 - Decorators.py
@@ -19,7 +19,7 @@ hi_func()
 bye_func( )
 '''
  
- g
+'''
 def decorator_function(original_function):
 	def wrapper_function(*args, **kwargs):
 		print('wrapper executed this before {}'.format(original_function.__name__))
@@ -29,7 +29,7 @@ def decorator_function(original_function):
 @decorator_function
 def display():
 	print('display function ran')
-
+'''
 
 '''
 @decorator_function 
@@ -84,13 +84,17 @@ def my_timer(orig_func):
 		result = orig_func(*args, **kwargs)
 		t2 = time.time() - t1
 		print('{} ran in: {} sec'.format(orig_func.__name__, t2))
-		
+		return result
+
+	return wrapper
+
+
 import time		
  
 @my_timer
 def display_info(name, age):
-	time.sleep(2)
+	time.sleep(1)
 	print('display info ran with arguments ({}, {})'.format(name, age))
 
 
-display_info('ifenium', 21)
+display_info('ife', 21)
